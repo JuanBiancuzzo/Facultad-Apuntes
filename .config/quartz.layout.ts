71c386b9d6c87df9e95dface510b72c51450e053
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2024-03-04 17:31:01 -0300

    Agregando configuración para la pagina

---
+++ .config/quartz.layout.ts
@@ -0,0 +1,56 @@
+import { PageLayout, SharedLayout } from "./quartz/cfg"
+import * as Component from "./quartz/components"
+
+// components shared across all pages
+export const sharedPageComponents: SharedLayout = {
+  head: Component.Head(),
+  header: [],
+  footer: Component.Footer({
+    links: {
+      GitHub: "https://github.com/JuanBiancuzzo/Facultad-Apuntes",
+    },
+  }),
+}
+
+// components for pages that display a single page (e.g. a single note)
+export const defaultContentPageLayout: PageLayout = {
+  beforeBody: [
+    Component.Breadcrumbs(),
+    Component.ArticleTitle(),
+    // Component.ContentMeta(),
+    // Component.TagList(),
+  ],
+  left: [
+    Component.PageTitle(),
+    Component.Spacer(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [
+    // Component.Graph(),
+    Component.Spacer(),
+    Component.Search(),
+    Component.TableOfContents(),
+    // Component.Darkmode(),
+    // Component.Backlinks(),
+    // Component.DesktopOnly(Component.TableOfContents()),
+  ],
+}
+
+// components for pages that display lists of pages  (e.g. tags or folders)
+export const defaultListPageLayout: PageLayout = {
+  beforeBody: [
+        Component.Breadcrumbs(), 
+        Component.ArticleTitle(), 
+        // Component.ContentMeta(),
+    ],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
+  right: [
+        Component.TableOfContents(),
+  ],
+}
