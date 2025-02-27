4d914114149baf0025a64ed12138cd6ca925422c
Juan Biancuzzo <72982017+JuanBiancuzzo@users.noreply.github.com>
2025-02-27 08:02:39 -0300

    Arreglando el tagsSeccion

--- _scripts/templater/obtenerTag.js
+++ _scripts/templater/obtenerTag.js
@@ -2,7 +2,7 @@ function obtenerTag(tp, tags) {
     if (!tags) return [];
 
     const { TAGS } = tp.user.constantes();
-    const tagsAFiltrar = [ ...tp.user.seccion().tagsSeccion(tp), TAGS.referencias, TAGS.nota.self ];
+    const tagsAFiltrar = [ ...tp.user.tagsSeccion(tp), TAGS.referencias, TAGS.nota.self ];
     return tags.filter(tag => tagsAFiltrar.every(tagAFiltrar => !tag.startsWith(tagAFiltrar)));
 }
 
