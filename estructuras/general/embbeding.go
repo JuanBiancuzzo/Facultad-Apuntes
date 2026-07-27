b3a610ec0a4da3b7453b2882a2b90d48bec95aa2
JuanBiancuzzo <jbiancuzzo@fi.uba.ar>
2026-07-27 12:46:33 -0300

    Corrigiendo el nombre de Embedding
    Antes habia hecho embbeding o embbedding

--- estructuras/general/embbeding.go
+++ estructuras/general/embbeding.go
@@ -8,45 +8,45 @@ import (
 )
 
 const TAM_FLOAT uint32 = 8
-const TAM_EMBBEDINGS uint32 = 384
-const TAM_EMBBEDINGS_BYTES uint32 = TAM_FLOAT * TAM_EMBBEDINGS
+const TAM_EMBEDDINGS uint32 = 384
+const TAM_EMBEDDINGS_BYTES uint32 = TAM_FLOAT * TAM_EMBEDDINGS
 
-type Embbeding struct {
-	embbeding []float64
+type Embedding struct {
+	embedding []float64
 }
 
-func NewEmbbeding(embbeding []byte) Embbeding {
-	float_embbeding := make([]float64, TAM_EMBBEDINGS)
+func NewEmbedding(embedding []byte) *Embedding {
+	float_embedding := make([]float64, TAM_EMBEDDINGS)
 	var i uint32
-	for i = 0; i < TAM_EMBBEDINGS; i++ {
-		bits := binary.LittleEndian.Uint64(embbeding[i * TAM_FLOAT:(i + 1) * TAM_FLOAT])
-		float_embbeding[i] = math.Float64frombits(bits)
+	for i = 0; i < TAM_EMBEDDINGS; i++ {
+		bits := binary.LittleEndian.Uint64(embedding[i * TAM_FLOAT:(i + 1) * TAM_FLOAT])
+		float_embedding[i] = math.Float64frombits(bits)
 	}
 
-	return Embbeding {
-		embbeding: float_embbeding,
+	return &Embedding {
+		embedding: float_embedding,
 	}
 }
 
-func (e Embbeding) ToString() string {
-	valores := make([]string, len(e.embbeding))
-	for i, valor := range e.embbeding {
+func (e Embedding) ToString() string {
+	valores := make([]string, len(e.embedding))
+	for i, valor := range e.embedding {
 		valores[i] = fmt.Sprintf("%.2f", valor) 
 	}
-	return fmt.Sprintf("Embbeding: [%s]", strings.Join(valores, ", "))
+	return fmt.Sprintf("Embedding: [%s]", strings.Join(valores, ", "))
 }
 
-func (e Embbeding) Comparar(otro Embbeding) float64 {
+func (e Embedding) Comparar(otro Embedding) float64 {
 	var (
 		resultado float64 = 0
 		norma_cuadrada float64 = 0
 		norma_cuadrada_otro float64 = 0
 	)
 
-	for i := range TAM_EMBBEDINGS {
-		resultado += e.embbeding[i] * otro.embbeding[i]
-		norma_cuadrada += e.embbeding[i]
-		norma_cuadrada_otro += otro.embbeding[i]
+	for i := range TAM_EMBEDDINGS {
+		resultado += e.embedding[i] * otro.embedding[i]
+		norma_cuadrada += e.embedding[i]
+		norma_cuadrada_otro += otro.embedding[i]
 	}
 
 	return resultado / math.Sqrt(norma_cuadrada * norma_cuadrada_otro)
